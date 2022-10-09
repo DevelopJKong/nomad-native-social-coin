c16cfb04d6cbfd88405e2385f854bcd9166ee5e8
DevelopJKong <ybaram40@gmail.com>
2022-10-09 16:27:48 +0900

    #6.6 Coins Screen 완료

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import auth from '@react-native-firebase/auth';
 import { NavigationContainer } from '@react-navigation/native';
 import InNav from './navigators/InNav';
 import OutNav from './navigators/OutNav';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -16,5 +19,9 @@ export default function App() {
       }
     });
   }, []);
-  return <NavigationContainer>{isLoggedIn ? <OutNav /> : <InNav />}</NavigationContainer>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>{isLoggedIn ? <OutNav /> : <InNav />}</NavigationContainer>
+    </QueryClientProvider>
+  );
 }
--- screens/Home.js
+++ screens/Home.js
@@ -1,13 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList } from 'react-native';
+import { useQuery } from 'react-query';
 import styled from 'styled-components/native';
+import { coins } from '../api';
 
 const Container = styled.View``;
-const Text = styled.Text``;
+const Loader = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Coin = styled.View`
+  align-items: center;
+  background-color: black;
+`;
+const CoinName = styled.Text`
+  color: white;
+`;
+const CoinSymbol = styled.Text`
+  color: white;
+`;
 
 const Home = () => {
+  const { isLoading, data } = useQuery('coins', coins);
+  const [cleanData, setCleanData] = useState([]);
+
+  useEffect(() => {
+    if (data) {
+      setCleanData(data.filter((coin) => coin.rank != 0 && coin.is_active && !coin.is_new));
+    }
+  }, [data]);
+
+  if (isLoading) {
+    return (
+      <Loader>
+        <ActivityIndicator color='white' size='large' />
+      </Loader>
+    );
+  }
+
   return (
     <Container>
-      <Text>Home</Text>
+      <FlatList
+        data={cleanData}
+        numColumns={5}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <Coin>
+            <CoinName>{item.name}</CoinName>
+            <CoinSymbol>{item.symbol}</CoinSymbol>
+          </Coin>
+        )}
+      />
     </Container>
   );
 };
