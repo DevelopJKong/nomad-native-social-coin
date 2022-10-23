56b0f280ac9d4996f42995db4204eb133d33cdd3
DevelopJKong <ybaram40@gmail.com>
2022-10-23 20:01:46 +0900

    #6.9 Victory Charts 완료

--- screens/Detail.js
+++ screens/Detail.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useQuery } from 'react-query';
 import styled from 'styled-components/native';
+import { VictoryChart, VictoryLine, VictoryScatter } from 'victory-native';
 import { history, info } from '../api';
 import { BLACK_COLOR } from '../color';
 
@@ -13,7 +14,7 @@ const Text = styled.Text``;
 const Detail = ({
   navigation,
   route: {
-    params: { symbol },
+    params: { symbol, id },
   },
 }) => {
   useEffect(() => {
@@ -23,13 +24,34 @@ const Detail = ({
   }, []);
 
   const { loading: infoLoading, data: infoData } = useQuery(['coinInfo', id], info);
-  const { loading: historyLoading, data: historyData } = useQuery(['coinHistory', id], history);
-
-  console.log(infoData);
-
+  const { isLoading: historyLoading, data: historyData } = useQuery(['coinHistory', id], history);
+  const [victoryData, setVictoryData] = useState(null);
+  useEffect(() => {
+    if (historyData) {
+      setVictoryData(
+        historyData.map((price) => ({
+          x: new Date(price.timestamp).getTime(),
+          y: price.price,
+        }))
+      );
+    }
+  }, [historyData]);
   return (
     <Container>
-      <Text>{symbol}</Text>
+      {victoryData ? (
+        <VictoryChart height={360}>
+          <VictoryLine
+            animate
+            interpolation="monotoneX"
+            data={victoryData}
+            style={{ data: { stroke: "#1abc9c" } }}
+          />
+          <VictoryScatter
+            data={victoryData}
+            style={{ data: { fill: "#1abc9c" } }}
+          />
+        </VictoryChart>
+      ) : null}
     </Container>
   );
 };
